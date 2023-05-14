2e4696466a1af9691a633f8718e9cb138f9462f6
febri wijaya <59653659+febriwijaya@users.noreply.github.com>
2023-05-14 13:26:25 +0700

    first commit
    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={LandingPage} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/parts/MostPicked.js
@@ -0,0 +1,45 @@
+import Button from "elements/Button";
+import React from "react";
+
+export default function MostPicked(props) {
+  return (
+    <section className="container">
+      <h4 className="mb-3">Most Picked</h4>
+      <div className="container-grid">
+        {props.data.map((items, index) => {
+          return (
+            <div
+              className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+            >
+              <div className="card card-featured">
+                <div className="tag">
+                  ${items.price}
+                  <span className="font-weight-light">per {items.unit}</span>
+                </div>
+                <figure className="img-wrapper">
+                  <img
+                    src={items.imageUrl}
+                    alt={items.name}
+                    className="img-cover"
+                  />
+                </figure>
+                <div className="meta-wrapper">
+                  <Button
+                    type="link"
+                    className="streched-link d-block text-white"
+                    href={`/properties/${items._id}`}
+                  >
+                    <h5>{items.name}</h5>
+                  </Button>
+                  <span>
+                    {items.city}. {items.country}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
