3dd5ec0cf4d9df034f0cd9d4ccb239af9e8fa587
febri wijaya <59653659+febriwijaya@users.noreply.github.com>
2023-07-05 21:19:15 +0700

    added detail page

--- my-app/src/parts/MostPicked.js
+++ my-app/src/parts/MostPicked.js
@@ -1,46 +1,53 @@
 import Button from "elements/Button";
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 export default function MostPicked(props) {
   return (
     <section className="container" ref={props.refMostPicked}>
-      <h4 className="mb-3">Most Picked</h4>
-      <div className="container-grid">
-        {props.data.map((items, index) => {
-          return (
-            <div
-              key={`mostpicked-${index}`}
-              className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
-            >
-              <div className="card card-featured">
-                <div className="tag">
-                  ${items.price}
-                  <span className="font-weight-light">per {items.unit}</span>
-                </div>
-                <figure className="img-wrapper">
-                  <img
-                    src={items.imageUrl}
-                    alt={items.name}
-                    className="img-cover"
-                  />
-                </figure>
-                <div className="meta-wrapper">
-                  <Button
-                    type="link"
-                    className="stretched-link d-block text-white"
-                    href={`/properties/${items._id}`}
-                  >
-                    <h5>{items.name}</h5>
-                  </Button>
-                  <span>
-                    {items.city}. {items.country}
-                  </span>
-                </div>
+      <Fade bottom>
+        <h4 className="mb-3">Most Picked</h4>
+        <div className="container-grid">
+          {props.data.map((items, index) => {
+            return (
+              <div
+                key={`mostpicked-${index}`}
+                className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+              >
+                <Fade bottom delay={500 * index}>
+                  <div className="card card-featured">
+                    <div className="tag">
+                      ${items.price}
+                      <span className="font-weight-light">
+                        per {items.unit}
+                      </span>
+                    </div>
+                    <figure className="img-wrapper">
+                      <img
+                        src={items.imageUrl}
+                        alt={items.name}
+                        className="img-cover"
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button
+                        type="link"
+                        className="stretched-link d-block text-white"
+                        href={`/properties/${items._id}`}
+                      >
+                        <h5>{items.name}</h5>
+                      </Button>
+                      <span>
+                        {items.city}. {items.country}
+                      </span>
+                    </div>
+                  </div>
+                </Fade>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      </Fade>
     </section>
   );
 }
