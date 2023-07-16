cc1d754f8d5cdaf2c1eef426f3089d402d69edf5
febri wijaya <59653659+febriwijaya@users.noreply.github.com>
2023-07-16 15:42:47 +0700

    added booking form

--- my-app/src/pages/DetailsPage.js
+++ my-app/src/pages/DetailsPage.js
@@ -5,6 +5,7 @@ import PageDetailTitle from "parts/PageDetailTitle";
 import ItemDetails from "../json/itemDetails.json";
 import FeaturedImage from "parts/FeaturedImage";
 import PageDetailDescription from "parts/PageDetailDescription";
+import BookingForm from "parts/BookingForm";
 
 export default class DetailsPage extends Component {
   componentDidMount() {
@@ -31,7 +32,9 @@ export default class DetailsPage extends Component {
             <div className="col-7 pr-5">
               <PageDetailDescription data={ItemDetails} />
             </div>
-            <div className="col-5">Booking Form</div>
+            <div className="col-5">
+              <BookingForm itemDetails={ItemDetails} />
+            </div>
           </div>
         </section>
       </>
